77788b84f0923b63c784b1ab73437437018cebfe
Irfan-Ahmad-byte <irfan.ahmad.mlka@gmail.com>
2023-04-03 13:31:52 +0500

    ude only TF for text rating

--- docsim.py
+++ docsim.py
@@ -1,25 +1,25 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 
 def rate_text(plavra, text):
-    # Create a TF-idf vectorizer object
-    vectorizer = TfidfVectorizer()
+    # Create a CountVectorizer object
+    vectorizer = CountVectorizer()
 
-    # Fit and transform the list of words and phrases and the description string into TF-idf matrices
+    # Fit and transform the list of words and phrases and the description string into term frequency matrices
     X = vectorizer.fit_transform(plavra + [text])
 
     # Get the feature names (words or phrases) from the vectorizer
     features = vectorizer.get_feature_names_out()
 
-    # Get the TF-idf matrix for the description string (the last row of X)
-    desc_tfidf = X[-1]
+    # Get the term frequency matrix for the description string (the last row of X)
+    desc_tf = X[-1]
 
     # Initialize a variable to store the rating score
     rating = 0
 
     # Loop over the list of words and phrases
     for i, word in enumerate(plavra):
-        # Get the TF-idf score for the word or phrase in the description string
-        score = desc_tfidf[0, i]
+        # Get the term frequency for the word or phrase in the description string
+        score = desc_tf[0, i]
         # Add the score to the rating
         rating += score
 
