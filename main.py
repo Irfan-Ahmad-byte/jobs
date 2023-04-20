9284e4cc15048afd297855e19d166b4a5fcbf236
Irfan-Ahmad-byte <irfan.ahmad.mlka@gmail.com>
2023-04-20 16:16:50 +0500

    updae

--- main.py
+++ main.py
@@ -254,7 +254,7 @@ def extractDescription(url):
   # Fetch the HTML content from the URL using requests library (or any other method)
   #logging.info('Getting job description from %s', url)
   try:
-    time.sleep(random.uniform(5))
+    time.sleep(5)
     res = requests.get(url, headers=headers, timeout=3)
     if res.status_code == 200:
       html = res.content
