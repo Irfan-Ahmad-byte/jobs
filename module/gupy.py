d7f86dba92c77712df5484a5f6ff34fb570bdbd8
Irfan-Ahmad-byte <irfan.ahmad.mlka@gmail.com>
2023-04-24 12:32:02 +0500

    99jobs

--- module/gupy.py
+++ module/gupy.py
@@ -71,7 +71,7 @@ class Gupy:
         location = f"{card['city']}, {card['state']}, {card['country']}"
         job_name = card["name"]
         description = card["description"]
-        posted_date = card["publishedDate"].split('T')
+        posted_date = card["publishedDate"].split('T')[0]
         job_url = card["jobUrl"]
 
         rating = 0
